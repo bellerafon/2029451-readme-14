047a23fbd1d1a28f4f15d7f8a5a27ad5555b5554
Abzal <abzal_or@mail.ru>
2022-05-16 07:19:58 +0600

    3.7. Работаем с датой

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+date_default_timezone_set('Asia/Almaty');
 require_once('helpers.php');
 require_once('data.php');
 
-$page_content = include_template('main.php', ['populars' => $populars]);
+$page_content = include_template('main.php', [
+    'populars' => $populars
+]);
 
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
